6be2a9f2ea350d90bc3c500c7ef2024218841b3d
phillyfan1138 <danstahl1138@gmail.com>
2015-11-08 09:55:45 -0500

    added readme

--- MarketRiskWrapper.js
+++ MarketRiskWrapper.js
@@ -111,17 +111,3 @@ var runFunc=function(arrayUrl, i){
 }
 runFunc(apiStrings, 0);
 https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD1WKD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json", callback); //1 week libor history
-//process.stdin.resume();
-
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD1MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 1 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD3MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD6MTD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 6 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=USD12MD156N&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 12 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP2&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 2 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP3&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 3 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP4&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 4 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP5&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 5 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP7&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 7 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP10&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 10 year swap on 3 month LIBOR
-//https.get("https://api.stlouisfed.org/fred/series/observations?series_id=DSWP30&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json&sort_order=desc&limit=1", clb); //most current 30 year swap on 3 month LIBOR
-//console.log(YieldCurveUnExtrapolated);
